f309ab991c543a319c415730fc816357ac105d5b
filip <filip.ciebiera@gmail.com>
2023-03-03 14:42:55 +0100

    nie

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner wejscie = new Scanner(System.in);
-        int liczba = wejscie.nextInt();
-        System.out.print(liczba + 3);
+        nieee a = new nieee();
+        a.numer = 2;
+        System.out.print(a.numer);
     }
 }
