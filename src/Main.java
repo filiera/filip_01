dde976889f42789f0540ccc9ec548b299f67033c
filip <filip.ciebiera@gmail.com>
2023-03-03 14:32:46 +0100

    nie

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner wejscie = new Scanner(System.in);
+        int liczba = wejscie.nextInt();
+        System.out.print(liczba + 3);
+    }
+}
